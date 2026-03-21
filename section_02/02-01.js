a89107beb672929c8bdeb52d420b8c36766ec1ba
chan9yu <dev.cgyeo@gmail.com>
2026-03-21 12:00:00 +0900

    add: 02-01.js

--- section_02/02-01.js
+++ section_02/02-01.js
@@ -20,7 +20,15 @@
  */
 
 function solution(arr) {
-	// 여기에 풀이를 작성하세요
+	const answer = [arr[0]];
+
+	for (let i = 1; i < arr.length; i++) {
+		if (arr[i - 1] < arr[i]) {
+			answer.push(arr[i]);
+		}
+	}
+
+	return answer;
 }
 
 console.log(solution([7, 3, 9, 5, 6, 12])); // 7 9 6 12
