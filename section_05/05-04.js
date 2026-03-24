ec7dd247913055640b21880b1d64dd8732f72b30
chan9yu <dev.cgyeo@gmail.com>
2026-03-24 12:00:00 +0900

    add: 05-04.js

--- section_05/05-04.js
+++ section_05/05-04.js
@@ -26,7 +26,22 @@
  */
 
 function solution(m, arr) {
-	// 여기에 풀이를 작성하세요
+	let answer = 0;
+	let sum = 0;
+	let left = 0;
+
+	for (let right = 0; right < arr.length; right++) {
+		sum += arr[right];
+
+		while (sum > m) {
+			sum -= arr[left];
+			left += 1;
+		}
+
+		answer += right - left + 1;
+	}
+
+	return answer;
 }
 
 console.log(solution(5, [1, 3, 1, 2, 3])); // 10
