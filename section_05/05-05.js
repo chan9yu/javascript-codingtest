2ecf74325889a73d2fb09b9073850645aa3f2bfd
chan9yu <dev.cgyeo@gmail.com>
2026-03-24 12:00:00 +0900

    add: 05-05.js

--- section_05/05-05.js
+++ section_05/05-05.js
@@ -25,7 +25,21 @@
  */
 
 function solution(k, arr) {
-	// 여기에 풀이를 작성하세요
+	let answer = 0;
+	let sum = 0;
+
+	for (let i = 0; i < k; i++) {
+		sum += arr[i];
+	}
+
+	answer = sum;
+
+	for (let i = k; i < arr.length; i++) {
+		sum += arr[i] - arr[i - k];
+		answer = Math.max(answer, sum);
+	}
+
+	return answer;
 }
 
 console.log(solution(3, [12, 15, 11, 20, 25, 10, 20, 19, 13, 15])); // 56
