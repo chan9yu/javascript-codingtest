1da6a3c79d563106d963dad9e34cda5a2f345451
chan9yu <dev.cgyeo@gmail.com>
2026-03-24 12:00:00 +0900

    add: 05-02.js

--- section_05/05-02.js
+++ section_05/05-02.js
@@ -26,7 +26,26 @@
  */
 
 function solution(arr1, arr2) {
-	// 여기에 풀이를 작성하세요
+	arr1.sort((a, b) => a - b);
+	arr2.sort((a, b) => a - b);
+
+	const answer = [];
+	let p1 = 0;
+	let p2 = 0;
+
+	while (p1 < arr1.length && p2 < arr2.length) {
+		if (arr1[p1] === arr2[p2]) {
+			answer.push(arr1[p1]);
+			p1 += 1;
+			p2 += 1;
+		} else if (arr1[p1] < arr2[p2]) {
+			p1 += 1;
+		} else {
+			p2 += 1;
+		}
+	}
+
+	return answer;
 }
 
 console.log(solution([1, 3, 9, 5, 2], [3, 2, 5, 7, 8])); // [2, 3, 5]
